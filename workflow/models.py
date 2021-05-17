e6b036c2ac6a7093c0082ecc8d4cdaf845dc7e18
iury <iuryfernandes.eng@gmail.com>
2021-05-17 18:55:30 -0300

    model do workflow

--- workflow/models.py
+++ workflow/models.py
@@ -1,3 +1,15 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class StatusChoice(models.TextChoices):
+    INSERTED = 'inserted'
+    CONSUMED = 'consumed'
+
+
+class Workflow(models.Model):
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=8, choices=StatusChoice.choices, default=False)
+    data = models.JSONField()
+    steps = ArrayField(ArrayField(models.CharField(max_length=100), blank=True))
