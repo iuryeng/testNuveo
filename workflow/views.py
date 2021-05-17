62ac36d19cd3549da239b8cb72c25fd990a4215d
iury <iuryfernandes.eng@gmail.com>
2021-05-17 19:12:14 -0300

    criação das views

--- workflow/views.py
+++ workflow/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class WorkflowViewSet(viewsets.ViewSet):
+    def list(self, request):  # api/workflow
+        pass
+
+    def create(self, request):  # api/workflow
+        pass
+
+    def retrieve(self, request, pk=None):  # api/workflow/{str:uuid}
+        pass
+
+    def partial_update(self, request):
+        pass
