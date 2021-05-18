70f05b450c1cac4a23563004f8054c990c62d8b1
iury <iuryfernandes.eng@gmail.com>
2021-05-18 20:40:36 -0300

    implementação de função de mensageria

--- consumer.py
+++ consumer.py
@@ -6,7 +6,6 @@ import pika
 
 import django
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiworkflow.settings")
 django.setup()
 
@@ -19,13 +18,19 @@ connection = pika.BlockingConnection(url_params)
 channel = connection.channel()
 
 channel.queue_declare(queue='workflow')
+channel.queue_declare(queue='workflow_csv')
 
 
-def callback(ch, method, properties, body):
-        print("recived in apiworkflow")
+def callback_inserted(ch, method, properties, body):
+    print("recived in apiworkflow")
 
 
-channel.basic_consume(queue='workflow', on_message_callback=callback, auto_ack=True)
+def callback_csv(ch, method, properties, body):
+    print('consumed workflow and generate csv')
+
+
+channel.basic_consume(queue='workflow', on_message_callback=callback_inserted, auto_ack=True, )
+channel.basic_consume(queue="workflow_csv", on_message_callback=callback_csv, auto_ack=True)
 
 print("Started Consuming")
 
