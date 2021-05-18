8d3b8aa27efa8124a766f6d4fa510c4b151d0e51
iury <iuryfernandes.eng@gmail.com>
2021-05-18 03:08:55 -0300

    configuracao publish() rmq

--- workflow/models.py
+++ workflow/models.py
@@ -10,7 +10,7 @@ class StatusChoice(models.TextChoices):
 
 class Workflow(models.Model):
     UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    status = models.CharField(max_length=8, choices=StatusChoice.choices, default=False)
+    status = models.CharField(max_length=8, choices=StatusChoice.choices, default='False', blank=True)
     data = models.JSONField()
     steps = ArrayField(base_field=models.CharField(max_length=200, null=True), default=list, blank=True)
 
