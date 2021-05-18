4aa90cf05857386b37306e626b53b5a104d94215
iury <iuryfernandes.eng@gmail.com>
2021-05-18 01:28:37 -0300

    teste publish() em create view

--- workflow/views.py
+++ workflow/views.py
@@ -1,7 +1,10 @@
+from _testcapi import raise_exception
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from workflow.models import Workflow
+from .inserted import publish
 from .serializers import WorkflowSerializer
 
 
@@ -15,13 +18,15 @@ class WorkflowViewSet(viewsets.ViewSet):
         serializer = WorkflowSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, request, pk=None):  # api/workflow/{str:uuid}
-        pass
+    def partial_update(self, request, pk=None):  # api/workflow/{str:uuid}
+        workflow = Workflow.objects.get(UUID=pk)
+        serializer = WorkflowSerializer(instance=workflow, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def partial_update(self, request):
-        pass
-
-    def destroy(self, request):
+    def workflow_consome(self, request):
         pass
