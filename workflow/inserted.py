746544df63a1db93018dcfc8749d08a4179ebefe
iury <iuryfernandes.eng@gmail.com>
2021-05-18 03:07:34 -0300

    configuração de inserted rmq

--- workflow/inserted.py
+++ workflow/inserted.py
@@ -1,3 +1,5 @@
+import json
+
 import pika
 
 params = pika.URLParameters('amqps://aufinhks:PaQoo2lak0mHCXFb3fzEiHy7q7-N2Daq@baboon.rmq.cloudamqp.com/aufinhks')
@@ -7,5 +9,5 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish(met):
-    channel.basic_publish(exchange='', routing_key='workflow', body=' hello recebido da fila!')
+def publish():
+    channel.basic_publish(exchange='', routing_key='workflow', body="inserido na fila")
