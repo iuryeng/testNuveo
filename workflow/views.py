15faf84de45246d45b2cb0b243ed41f0a4bad206
iury <iuryfernandes.eng@gmail.com>
2021-05-17 23:49:40 -0300

    implementacao de list e create

--- workflow/views.py
+++ workflow/views.py
@@ -1,15 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from workflow.models import Workflow
+from .serializers import WorkflowSerializer
 
 
 class WorkflowViewSet(viewsets.ViewSet):
     def list(self, request):  # api/workflow
-        pass
+        workflow = Workflow.objects.all()
+        serializer = WorkflowSerializer(workflow, many=True)
+        return Response(serializer.data)
 
     def create(self, request):  # api/workflow
-        pass
+        serializer = WorkflowSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):  # api/workflow/{str:uuid}
         pass
 
     def partial_update(self, request):
+        pass
+
+    def destroy(self, request):
         pass
