2319b2a669147bf7a229ad5c2dc6e1513184cc22
iury <iuryfernandes.eng@gmail.com>
2021-05-22 02:14:55 -0300

    implementação de testes unitários

--- workflow/tests.py
+++ workflow/tests.py
@@ -1,3 +1,26 @@
+import mock
+from django.db import models
 from django.test import TestCase
+from workflow.models import Workflow
 
-# Create your tests here.
+
+
+class TestWorkflow(TestCase):
+
+    def test_field_uuid(self):
+        field = Workflow._meta.get_field('UUID')
+        self.assertTrue(isinstance(field, models.UUIDField))
+        self.assertTrue(field.primary_key)
+
+    def test_uuid_is_unique(self):
+        unique = Workflow._meta.get_field('UUID').unique
+        self.assertEquals(unique, True)
+
+
+    def test_field_updated_at(self):
+        field = Workflow._meta.get_field('data')
+        self.assertTrue(isinstance(field, models.JSONField))
+
+
+
+
